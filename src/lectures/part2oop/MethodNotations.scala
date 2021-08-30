f7807c63918b00a63a192bc09734ff1cd5959b87
Brais Cidras <braisomary@gmail.com>
2021-08-30 16:49:43 +0200

    Method Notations (Exercises)

--- src/lectures/part2oop/MethodNotations.scala
+++ src/lectures/part2oop/MethodNotations.scala
@@ -4,16 +4,24 @@ import scala.language.postfixOps
 
 object MethodNotations extends App {
 
-  class Person(val name: String, favoriteMovie: String) {
+  class Person(val name: String, favoriteMovie: String, age: Int = 0) {
     def likes(movie: String) = movie == favoriteMovie
 
     //def hangOutWith(person: Person) = s"${this.name} is hanging out with ${name}"
-    def +(person: Person) = s"${this.name} is hanging out with ${name}"
+    def +(person: Person) = s"${this.name} is hanging out with ${person.name}"
+    def +(nickname: String) = new Person( s"${name} (${nickname})", favoriteMovie)
+
+    def learns(subject: String) = s"${name} learns ${subject}"
+    // Equivalent def learns: String = this learns "Scala"
+    // Equivalent def learns: String = learns("Scala")
 
     def unary_! = s"$name, what the heck?!"
+    def unary_+ = new Person(name, favoriteMovie, age + 1)
 
     def isAlive = true
+    def is = s"${name} is ${age} years old"
     def apply() = s"Hi, my name is $name and I like $favoriteMovie"
+    def apply(n: Int) = s"$name watched $favoriteMovie $n times"
   }
 
   val mary = new Person("Mary", "Inception")
@@ -49,4 +57,30 @@ object MethodNotations extends App {
   // Apply
   println(mary.apply())
   println(mary()) // equivalent, it's looking for a definition of apply inside the class
+
+  /*
+    1.  Overload the + operator
+        mary + "the rockstar" => new person "Mary (the rockstar)"
+
+    2.  Add age to the Person class
+        Add an unary + operator => new person with the age + 1
+        +mary => mary with the age incremented
+
+    3.  Add a "learns" method in the Person class => "Mary learns Scala"
+        Add a learnScala method, calls learns method with "Scala".
+        Use it in postfix notation.
+
+    4.  Overload the apply method
+        mary.apply(2) => "Mary watched Inception 2 times"
+   */
+
+  println((mary + "the rockstar")())
+
+  println(mary is)
+  println(+mary is)
+
+  println(mary learns "Python")
+  println(mary learns)
+  println(mary(2))
+
 }
